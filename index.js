7c25f66ac64e0f3195d2b74dc09de8a067fc112e
unknown <ebussman@gmail.com>
2022-10-27 16:43:24 -0700

    Moved pathing around so the json data is actually getting read now

--- index.js
+++ index.js
@@ -11,34 +11,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,"./views/index.html"))
+    res.sendFile(path.join(__dirname,"./public/index.html"))
 })
 
 // GET /notes should return the notes.html file
 app.get('/notes', (req,res) =>{
+    console.log("hello");
+    
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 });
 
-// GET * should return the index.html file
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-});
-
 // GET /api/notes should read the db.json file
-app.get('/api/notes', (req,res) => {
-    fs.readFile("./db/db.json", "utf-8", (err,data) => {
+app.get('/api/notes',(req,res)=>{
+    fs.readFile("./db/db.json","utf-8",(err,data)=>{
         if(err){
             console.log(err);
             res.status(500).json({
                 msg:"oh no!",
                 err:err
-            })     
+            })
         } else {
             const dataArr = JSON.parse(data);
-            res.json(dataArr);
+            res.json(dataArr)
         }
     })
-})
+});
+
+// GET * should return the index.html file
+app.get('*', (req,res) =>{
+    console.log("catch all log");
+    
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+});
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 app.post('/api/notes', (req,res) =>{
@@ -67,4 +71,8 @@ app.post('/api/notes', (req,res) =>{
             })
         }
     })
+})
+
+app.listen(PORT,()=>{
+    console.log(`listening on port ${PORT}`)
 })
