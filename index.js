a354751d128c833d52692334a746e022261c5e61
unknown <ebussman@gmail.com>
2022-10-27 17:15:16 -0700

    Trying smth out to post notes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
+const uuid = require('uuid');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"))
@@ -37,41 +38,66 @@ app.get('/api/notes',(req,res)=>{
     })
 });
 
-// GET * should return the index.html file
-app.get('*', (req,res) =>{
-    console.log("catch all log");
-    
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-});
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-app.post('/api/notes', (req,res) =>{
-    fs.readFile("./db/db.json", "utf-8", (err,data) => {
-        if(err){
-            console.log(err);
-            res.status(500).json({
-                msg:"oh no!",
-                err:err
-            })     
-        } else {
-            const dataArr = JSON.parse(data);
-            dataArr.push(req.body);
-            fs.writeFile("./db/db.json", JSON.stringify(dataArr, null, 4), (err,data)=>{
-                if(err){
-                    console.log(err);
-                    res.status(500).json({
-                        msg:"oh no!",
-                        err:err
-                    })     
-                } else {
-                    res.json({
-                        msg:"successfully added!"
-                    })
-                }
-            })
-        }
-    })
-})
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        review_id: uuid(),
+      };
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting note');
+    }
+});
+// app.post('/api/notes', (req,res) =>{
+//     fs.readFile("./db/db.json", "utf-8", (err,data) => {
+//         if(err){
+//             console.log(err);
+//             res.status(500).json({
+//                 msg:"oh no!",
+//                 err:err
+//             })     
+//         } else {
+//             const dataArr = JSON.parse(data);
+//             dataArr.push(req.body);
+//             fs.writeFile("./db/db.json", JSON.stringify(dataArr, null, 4), (err,data)=>{
+//                 if(err){
+//                     console.log(err);
+//                     res.status(500).json({
+//                         msg:"oh no!",
+//                         err:err
+//                     })     
+//                 } else {
+//                     res.json({
+//                         msg:"successfully added!"
+//                     })
+//                 }
+//             })
+//         }
+//     })
+// })
+// GET * should return the index.html file
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+});
 
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`)
